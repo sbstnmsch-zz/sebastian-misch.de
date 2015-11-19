c809892f1358ee01f94ff4028f27b65aca41201b
smisch <sebastian.misch@epost-dev.de>
2015-11-19 15:01:25 +0100

    Removed node 0.10 support

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,6 +1,7 @@
 (() => {
   let
-    night = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].indexOf((new Date()).getHours()) === -1;
+    hours = (new Date()).getHours(),
+    night = !(8 < hours === hours < 18);
 
   if (night) {
     window.document.body.classList.add('u-night');
