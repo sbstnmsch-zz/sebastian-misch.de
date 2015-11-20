9f1130217ced0814c4fce6ee2c76356e85e3a7bf
smisch <sebastian.misch@epost-dev.de>
2015-11-20 09:26:47 +0100

    Fix page load time

--- src/javascripts/util/performance-visualizer.js
+++ src/javascripts/util/performance-visualizer.js
@@ -8,7 +8,7 @@ class PerformanceVisualizer {
         domEl = _window.document.querySelector('.app-performance-dom .app-performance-bubble-value');
 
       if (loadEl) {
-        loadEl.innerText = t.loadEventEnd - t.responseEnd;
+        loadEl.innerText = t.responseStart - t.responseEnd;
       }
 
       if (domEl) {
