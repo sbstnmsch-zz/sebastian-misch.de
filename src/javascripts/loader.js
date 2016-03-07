978f9592bf3f6e2ddb26d4f3de29d1c82ff3164c
Sebastian Misch <sbstnmsch@gmail.com>
2016-03-07 19:53:36 +0100

    Add more robustness to async asset loader

--- src/javascripts/loader.js
+++ src/javascripts/loader.js
@@ -6,7 +6,7 @@ window.onload = () => {
     let sct = window.document.createElement('script');
     sct.type = 'text/javascript';
     sct.async = true;
-    sct.src = `${window.cdn}/assets/javascripts/${script}`;
+    sct.src = `${window.cdn || ''}/assets/javascripts/${script}`;
     (window.document.getElementsByTagName('head')[0]).appendChild(sct);
   });
 
@@ -14,7 +14,7 @@ window.onload = () => {
     let sct = window.document.createElement('link');
     sct.rel = 'stylesheet';
     sct.async = true;
-    sct.href = `${window.cdn}/assets/stylesheets/${style}`;
+    sct.href = `${window.cdn || ''}/assets/stylesheets/${style}`;
     (window.document.getElementsByTagName('head')[0]).appendChild(sct);
   });
 };
