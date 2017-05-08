211783f8b31f86cfdb6d995b164df0a48b8bf5ff
Sebastian Misch <sebastian.misch@westernacher.com>
2017-05-08 21:48:12 +0200

    Add new site

---
+++ README.md
@@ -0,0 +1,30 @@
+[![Build Status](https://api.travis-ci.org/sbstnmsch/sebastian-misch.de.svg?branch=master)](https://travis-ci.org/sbstnmsch/sebastian-misch.de)
+
+# My personal website
+live at https://sebastian-misch.de - feel free to use for your own.
+
+## Install
+```
+$ npm install
+```
+
+## Development
+To watch your sources and rebuild automatically type:
+```
+$ npm start
+```
+Open http://localhost:8080 with your favorite browser.
+
+## Production build
+Build, optimize and minimize all assets.
+```
+$ npm run build
+```
+
+## Deploy via CI
+A travis-ci compliant Yaml is included.
+See https://www.npmjs.com/package/sftp-deploy on how to set this up.
+
+```
+$ npm run deploy
+```
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const extractStyles = new ExtractTextPlugin('[name]-[hash].css');
+
+module.exports = {
+  stats: {
+    children: false,
+    hash: false,
+    reasons: false,
+    version: false
+  },
+  entry: {
+    app: './src/app/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]-[hash].js'
+  },
+  module: {
+    rules: [{
+      test: /\.html$/,
+      use: ['html-loader']
+    }, {
+      test: /\.scss$/,
+      use: extractStyles.extract({
+        use: ['css-loader', 'postcss-loader', 'sass-loader']
+      })
+    }]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: false,
+      options: {
+        postcss: [
+          autoprefixer({
+            browsers: [
+              'last 3 Android versions',
+              'last 4 iOS versions',
+              'last 5 Chrome versions',
+              'last 8 ChromeAndroid versions',
+              'last 4 ExplorerMobile versions',
+              'last 5 FirefoxAndroid versions'
+            ]
+          })
+        ]
+      }
+    }),
+    extractStyles,
+    new webpack.optimize.UglifyJsPlugin({}),
+    new HtmlWebpackPlugin({
+      template: 'src/html/index.html',
+      inject: 'body'
+    })
+  ]
+};
