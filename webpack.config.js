4c2fdc9b73b493ee0bf34c6f30589d6e64f54816
Sebastian Misch <sbstnmsch@gmail.com>
2018-02-27 10:52:16 +0100

    Fix for webpack@4

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('path');
-const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const autoprefixer = require("autoprefixer");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const extractStyles = new ExtractTextPlugin('[name]-[hash].css');
+const extractStyles = new ExtractTextPlugin("[name]-[hash].css");
 
 module.exports = {
   stats: {
@@ -14,54 +14,59 @@ module.exports = {
     version: false
   },
   entry: {
-    app: './src/app/index.js',
+    app: "./src/app/index.js"
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name]-[hash].js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name]-[hash].js"
   },
   module: {
     rules: [
       {
         test: /\.html$/,
-        use: ['html-loader']
+        use: ["html-loader"]
       },
       {
         test: /\.(jpg|png)$/,
-        use: ['file-loader']
+        use: ["file-loader"]
       },
       {
         test: /\.scss$/,
         use: extractStyles.extract({
-          use: ['css-loader', 'postcss-loader', 'sass-loader']
+          use: [
+            {
+              loader: "css-loader"
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                plugins: () => [
+                  autoprefixer({
+                    browsers: [
+                      "last 3 Android versions",
+                      "last 4 iOS versions",
+                      "last 5 Chrome versions",
+                      "last 8 ChromeAndroid versions",
+                      "last 4 ExplorerMobile versions",
+                      "last 5 FirefoxAndroid versions"
+                    ]
+                  })
+                ]
+              }
+            },
+            {
+              loader: "sass-loader"
+            }
+          ]
         })
       }
     ]
   },
   plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: false,
-      options: {
-        postcss: [
-          autoprefixer({
-            browsers: [
-              'last 3 Android versions',
-              'last 4 iOS versions',
-              'last 5 Chrome versions',
-              'last 8 ChromeAndroid versions',
-              'last 4 ExplorerMobile versions',
-              'last 5 FirefoxAndroid versions'
-            ]
-          })
-        ]
-      }
-    }),
     extractStyles,
-    new webpack.optimize.UglifyJsPlugin({}),
-    // new CopyWebpackPlugin([{ from: 'src/static/png', to: 'assets' }]),
     new HtmlWebpackPlugin({
-      template: 'src/html/index.html',
-      inject: 'body'
+      template: "src/html/index.html",
+      inject: "body"
     })
   ]
 };
